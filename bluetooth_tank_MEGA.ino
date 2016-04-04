42a4cab089fa1de3d45ff29bf184075941e61dac
Kaan Göksal <kaangoksal@gmail.com>
2016-04-04 10:42:09 -0400

    Update bluetooth_tank_MEGA.ino

--- bluetooth_tank_MEGA.ino
+++ bluetooth_tank_MEGA.ino
@@ -1,21 +1,34 @@
+/** Created by Kaan Göksal 
+ * 4/4/2016 
+ * This code is used to control a rc vehicle with 
+ * L298N + HC06 + Arduino MEGA
+ *  
+ * Kaan Göksal yazmıştır bu kodu.
+ * 4 Nisan 2016
+ * Bu kod uzaktan kumandalı bir aracı kontrol eder.
+ * L298N + HC06 + Arduino MEGA ile uyumludur! 
+ * Serial.print ifadeleri debugging için kullanılmaktadır, silebilirsiniz.
+ * 
+ * 
+ */
 
 
-int IN1 = 8;
-int IN2 = 9;
-int IN3 = 10;
-int IN4 = 11;
+int IN1 = 8; // Motor 1
+int IN2 = 9; // Motor 1
+int IN3 = 10; //Motor 2
+int IN4 = 11; //Motor 2
 
 
 void setup()
 {
  
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(9600); //HC-06 Modulu ile iletisim baslar - opening the comms for hc-06 module
  
- pinMode(IN1, OUTPUT);  //right motor
+ pinMode(IN1, OUTPUT);  //sağ motor - right motor 
  pinMode(IN2, OUTPUT);  
  pinMode(IN3, OUTPUT);
- pinMode(IN4, OUTPUT);  //left motor
+ pinMode(IN4, OUTPUT);  //sol motor - left motor
  
   
 }
@@ -25,7 +38,7 @@ void loop()
   String input = "";
   Serial.println("program start");
   
-     digitalWrite(IN1, LOW);
+     digitalWrite(IN1, LOW); //öncelikle motorları durduruyoruz! - first we stop the motors! 
      digitalWrite(IN2, LOW); 
      digitalWrite(IN3, LOW);
      digitalWrite(IN4, LOW); 
